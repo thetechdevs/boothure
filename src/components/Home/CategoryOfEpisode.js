0a32c6acf97f838326a4ca2633ee48f1fbfa398e
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-09-20 13:36:52 +0800

    data and some helper

--- src/components/Home/CategoryOfEpisode.js
+++ src/components/Home/CategoryOfEpisode.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import './Home.css';
 
+// images
+import BoothFM from '../../img/booth.png';
+import BoothDotCom from '../../img/booth.png';
+
+const CategoryOfEpisodeTemplate = ({ image, title, description }) => {
+    return (
+        <section className='categoryOfEpTemp'>
+
+        </section>
+    )
+};
+
 const CategoryOfEpisode = () => {
     return (
         <section className='categoryOfEp'>
-            
+            <CategoryOfEpisodeTemplate
+                image={BoothFM}
+                title='Booth FM'
+                description='Here you can find all booth fm'
+            />
+            <CategoryOfEpisodeTemplate
+                image={BoothDotCom}
+                title='Booth Dot Com'
+                description='Here you can find all booth dot com'
+            />
         </section>
     )
-}
+};
 
 export default CategoryOfEpisode;
---
+++ src/data/data.js
@@ -0,0 +1,234 @@
+const data = {
+    // home page
+    home: {
+        header: {},
+        latest5Episode: {},
+        categoryOfEpisode: {},
+        footer: {},
+    },
+
+    // boothFM page
+    boothFM: {
+        Y2010: {
+            January: {},
+            February: {},
+            March: {},
+            April: {},
+            May: {},
+            June: {},
+            July: {},
+            August: {},
+            September: {},
+            October: {},
+            November: {},
+            December: {}
+        },
+        Y2011: {
+            January: {},
+            February: {},
+            March: {},
+            April: {},
+            May: {},
+            June: {},
+            July: {},
+            August: {},
+            September: {},
+            October: {},
+            November: {},
+            December: {}
+        },
+        Y2012: {
+            January: {},
+            February: {},
+            March: {},
+            April: {},
+            May: {},
+            June: {},
+            July: {},
+            August: {},
+            September: {},
+            October: {},
+            November: {},
+            December: {}
+        },
+        Y2013: {
+            January: {},
+            February: {},
+            March: {},
+            April: {},
+            May: {},
+            June: {},
+            July: {},
+            August: {},
+            September: {},
+            October: {},
+            November: {},
+            December: {}
+        },
+        Y2014: {
+            January: {},
+            February: {},
+            March: {},
+            April: {},
+            May: {},
+            June: {},
+            July: {},
+            August: {},
+            September: {},
+            October: {},
+            November: {},
+            December: {}
+        },
+        Y2015: {
+            January: {},
+            February: {},
+            March: {},
+            April: {},
+            May: {},
+            June: {},
+            July: {},
+            August: {},
+            September: {},
+            October: {},
+            November: {},
+            December: {}
+        },
+        Y2016: {
+            January: {},
+            February: {},
+            March: {},
+            April: {},
+            May: {},
+            June: {},
+            July: {},
+            August: {},
+            September: {},
+            October: {},
+            November: {},
+            December: {}
+        },
+        Y2017: {
+            January: {},
+            February: {},
+            March: {},
+            April: {},
+            May: {},
+            June: {},
+            July: {},
+            August: {},
+            September: {},
+            October: {},
+            November: {},
+            December: {}
+        },
+        Y2018: {
+            January: {},
+            February: {},
+            March: {},
+            April: {},
+            May: {},
+            June: {},
+            July: {},
+            August: {},
+            September: {},
+            October: {},
+            November: {},
+            December: {}
+        },
+        Y2019: {
+            January: {},
+            February: {},
+            March: {},
+            April: {},
+            May: {},
+            June: {},
+            July: {},
+            August: {},
+            September: {},
+            October: {},
+            November: {},
+            December: {}
+        }
+    },
+    
+    // boothDotCom page
+    boothDotCom: {
+        Y2020: {
+            January: {},
+            February: {},
+            March: {},
+            April: {},
+            May: {},
+            June: {},
+            July: {},
+            August: {},
+            September: {},
+            October: {},
+            November: {},
+            December: {}
+        },
+        Y2021: {
+            January: {},
+            February: {},
+            March: {},
+            April: {},
+            May: {},
+            June: {},
+            July: {},
+            August: {},
+            September: {},
+            October: {},
+            November: {},
+            December: {}
+        },
+        Y2022: {
+            January: {},
+            February: {},
+            March: {},
+            April: {},
+            May: {},
+            June: {},
+            July: {},
+            August: {},
+            September: {},
+            October: {},
+            November: {},
+            December: {}
+        },
+        Y2023: {
+            January: {},
+            February: {},
+            March: {},
+            April: {},
+            May: {},
+            June: {},
+            July: {},
+            August: {},
+            September: {},
+            October: {},
+            November: {},
+            December: {}
+        },
+        Y2024: {
+            January: {},
+            February: {},
+            March: {},
+            April: {},
+            May: {},
+            June: {},
+            July: {},
+            August: {},
+            September: {},
+            October: {},
+            November: {},
+            December: {}
+        }
+    },
+
+    // pageNotFound page
+    pageNotFound: {
+
+    }
+};
+
+export default data;
