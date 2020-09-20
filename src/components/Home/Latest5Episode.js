8b66587a019f5c5114295eb21931c8170e32d5cd
proashik <ashik.swfu@outlook.com>
2020-09-21 02:18:54 +0800

    latest5 and category image work ok

--- src/components/Home/Latest5Episode.js
+++ src/components/Home/Latest5Episode.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './Home.css';
 
+// image
+import vhootHand from '../../img/vhoot-hand.jpg'
+import download from '../../img/Downloads-icon.png'
+
 const Latest5EpisodeTemplate = ({ image, title, data, btn, audio }) => {
     return (
         <section className='latest5EpTemp'>
                 <div className='latest5Eptemp__containerDiv latest5EpTemp__flex'>
                     <div className='latest5Eptemp__img'>
-                        <img src="https://resize.indiatvnews.com/en/resize/newbucket/715_-/2017/11/shutterstock-185032865-700x467-1509523022.jpg" alt="img"/>
+                        <img src={vhootHand} alt="img"/>
                     </div>
                     
                     <div className='latest5EpTemp__DetailSection'>
                         <h2 className='latest5EpTemp__title'><strong>Vhoot.com</strong></h2>
                         <p>date: 20/07/2020</p>
                         <div className='latest5EpTemp__flex'>
-                            <img className='latest5EpTemp__downloadIcon' src="https://icons.iconarchive.com/icons/dtafalonso/android-lollipop/512/Downloads-icon.png" alt="download"/>
+                            <img className='latest5EpTemp__downloadIcon' src={download} alt="download"/>
                               
                             {/* <audio controls >
                                 <source src='' type='audio/ogg'/>
