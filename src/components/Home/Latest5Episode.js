b4ae3f662195e69782586f03ab633348977510c5
proashik <ashik.swfu@outlook.com>
2020-09-21 00:06:13 +0800

    latest5episode

--- src/components/Home/Latest5Episode.js
+++ src/components/Home/Latest5Episode.js
@@ -4,7 +4,26 @@ import './Home.css';
 const Latest5EpisodeTemplate = ({ image, title, data, btn, audio }) => {
     return (
         <section className='latest5EpTemp'>
-
+                <div className='latest5Eptemp__containerDiv latest5EpTemp__flex'>
+                    <div className='latest5Eptemp__img'>
+                        <img src="https://resize.indiatvnews.com/en/resize/newbucket/715_-/2017/11/shutterstock-185032865-700x467-1509523022.jpg" alt="img"/>
+                    </div>
+                    
+                    <div className='latest5EpTemp__title'>
+                        <h2><strong>Vhoot.com</strong></h2>
+                        <p>date: 20/07/2020</p>
+                        <div className='latest5EpTemp__flex'>
+                            <img className='latest5EpTemp__downloadIcon' src="https://icons.iconarchive.com/icons/dtafalonso/android-lollipop/512/Downloads-icon.png" alt="download"/>
+                              
+                            {/* <audio controls >
+                                <source src='' type='audio/ogg'/>
+                                <source src='' type='audio/mpeg'/>
+                            </audio> */}         
+                        </div>
+                        
+                    </div>
+                </div>
+                
         </section>
     )
 };
@@ -12,6 +31,8 @@ const Latest5EpisodeTemplate = ({ image, title, data, btn, audio }) => {
 const Latest5Episode = () => {
     return (
         <section className='latest5Ep'>
+            <Latest5EpisodeTemplate />
+            <Latest5EpisodeTemplate />
             <Latest5EpisodeTemplate />
         </section>
     )
